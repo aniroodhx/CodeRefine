9aa5a40a3b109e744face106611f1f70935afbe1
aniroodh <aniroodh@amazon.com>
2026-08-11 15:26:27 +0000

    set up the project structure, 4 gradle modules to start

---
+++ coderefine-cli/src/main/java/com/coderefine/cli/CodeRefineApplication.java
@@ -0,0 +1,12 @@
+package com.coderefine.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeRefineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeRefineApplication.class, args);
+    }
+}
